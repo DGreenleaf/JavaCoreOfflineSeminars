93c65c840d40ab1a31cd4b5c4a10b817da645cbf
DGreenleaf <serg.savitskiy@gmail.com>
2016-08-25 01:08:37 +0300

    added working version for homework

--- src/homework/Module2HomeWork1.java
+++ src/homework/Module2HomeWork1.java
@@ -7,65 +7,78 @@ public class Module2HomeWork1 {
     public static void main(String[] args) {
 
         int[] newArray = {-14, 21, 32, 4, 54, 61, -7, 88, 9, -12};
-        sum(newArray);
-        max(newArray);
-        min(newArray);
-        maxPositive(newArray);
+        double[] newArrayD = {-14, 21, 32, 4, 54, 61, -7, 88, 9, -12};
 
-        long[] newArray2 = {-14, 21, 32, 4, 54, 61, -7, 88, 9, -12};
 
-        multiplication(newArray2);
+        System.out.println("Total is " + sum(newArray));
+
+        System.out.println("Maximum number is " + max(newArray));
+
+        System.out.println("Minimal number is " + min(newArray));
+
+        System.out.println("Maximum positive number is " + maxPositive(newArray));
+
+        double multi = multiplication(newArrayD);
+        System.out.println("The result of multiplying of array values is " + multi);
 
 
     }
 
-    public static void sum(int[] newArray) {
+    public static int sum(int[] newArray) {
 
         int sum = 0;
         for (int i = 0; i < newArray.length; i++) {
             sum = sum + newArray[i];
         }
-        System.out.println("Total is " + sum);
+        return sum;
     }
 
+    public static double sum(double[] newArray) {
 
-    public static void max(int[] newArray) {
+        double sum = 0;
+        for (int i = 0; i < newArray.length; i++) {
+            sum = sum + newArray[i];
+        }
+        return sum;
+    }
+
+    public static int max(int[] newArray) {
 
         int max = newArray[0];
         for (int i = 0; i < newArray.length; i++) {
             if (max < newArray[i])
                 max = newArray[i];
         }
-        System.out.println("Maximum number is " + max);
+        return max;
     }
 
-    public static void min(int[] newArray) {
+    public static int min(int[] newArray) {
 
         int min = newArray[0];
         for (int i = 0; i < newArray.length; i++) {
             if (min > newArray[i])
                 min = newArray[i];
         }
-        System.out.println("Minimal number is " + min);
+        return min;
     }
 
-    public static void maxPositive(int[] newArray) {
+    public static int maxPositive(int[] newArray) {
 
-        int maxPosititive = newArray[0];
+        int maxPositive = newArray[0];
         for (int i = 0; i < newArray.length; i++) {
-            if (maxPosititive < newArray[i] && newArray[i] > 0)
-                maxPosititive = newArray[i];
+            if (maxPositive < newArray[i] && newArray[i] > 0)
+                maxPositive = newArray[i];
         }
-        System.out.println("Maximum positive number is " + maxPosititive);
+        return maxPositive;
     }
 
-    public static void multiplication(long[] newArray2) {
+    public static long multiplication(double[] newArrayD) {
 
         long multiplication = 1;
-        for (long value : newArray2) {
-            multiplication *= value;
+        for (double value : newArrayD) {
+            multiplication *= (long)value;
         }
-        System.out.println("The result of multiplying of array values is " + multiplication);
+        return multiplication;
     }
 
 
