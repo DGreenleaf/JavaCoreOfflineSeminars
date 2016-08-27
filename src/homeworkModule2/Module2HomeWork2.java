f4b39e83f9c197d87122f2273b9e8ef052435157
DGreenleaf <serg.savitskiy@gmail.com>
2016-08-27 18:57:29 +0300

    changes to task 2.2

--- src/homeworkModule2/Module2HomeWork2.java
+++ src/homeworkModule2/Module2HomeWork2.java
@@ -1,4 +1,4 @@
-package homework;
+package homeworkModule2;
 
 public class Module2HomeWork2 {
 
@@ -8,9 +8,9 @@ public class Module2HomeWork2 {
 
     // double balance и withdrawal - переменные
 
-    static double withdraw(double balance, double withdrawal, double commission) {
+    static double withdraw(double balance, double withdrawal, double commissionFinal) {
         // double commission = 5% = 0,05;
-        return balance - withdrawal - (commission * withdrawal);
+        return balance - withdrawal - commissionFinal;
     }
 
     public static void main(String[] args) {
@@ -18,17 +18,29 @@ public class Module2HomeWork2 {
         // balance - сумма денег на счету
         double balance = 150;
         // withdraw - то, что снимаем со счета
-        double withdraw = 10.3;
+        double firstWithdraw = 10.3;
+        double secondWithdraw = 150;
         // commission - комиссия при снятии
         double commission = 0.05;
         // можно сразу писать численные значения вместо методов (balance, withdraw, commission)
 
-        double balanceAfter = withdraw(balance, withdraw, commission);
-        double comissionFinal = commission * withdraw;
+        double balanceAfter = withdraw(balance, firstWithdraw, commission);
+        double balanceAfter2 = withdraw(balanceAfter, secondWithdraw, commission);
+        double commissionFinal = commission * firstWithdraw;
+        double commissionFinal2 = commission * secondWithdraw;
 
         if(balanceAfter >= 0) {
             // ok - выдается если хватает денег на снятие указанной суммы с учетом комиссии
-            System.out.println("ok " + " Discounted comission = " + comissionFinal + " Remaining balance = " + balanceAfter);
+            System.out.println("ok " + " Discounted comission = " + commissionFinal + " Remaining balance = " + balanceAfter);
+        }
+
+        else {
+            System.out.println("no");
+        }
+
+        if(balanceAfter2 >= 0) {
+            // ok - выдается если хватает денег на снятие указанной суммы с учетом комиссии
+            System.out.println("ok " + " Discounted comission = " + commissionFinal2 + " Remaining balance = " + balanceAfter2);
         }
 
         else {
