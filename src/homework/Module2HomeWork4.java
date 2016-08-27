f667c4c4ad38967d4518a42679e0f91da893bc2a
DGreenleaf <serg.savitskiy@gmail.com>
2016-08-27 18:20:07 +0300

    completed task 2.4

---
+++ src/homework/Module2HomeWork4.java
@@ -0,0 +1,21 @@
+package homework;
+
+public class Module2HomeWork4 {
+    static double withdraw(double balance, double withdrawal, double commissionFinal) {
+        // double commission = 5% = 0,05;
+        return balance - withdrawal - commissionFinal;
+    }
+
+    public static void main(String[] args) {
+
+        int[] balances = {120, 390, 240, 1500, 200, 90000, 1100, 20, 0, 100};
+        String[] ownerNames = {"Sergii ", "John ", "Viktor ", "Jackson ", "Lasley ", "Pol ", "Ted ", "Manny ", "Rocky ", "Terry "};
+        double recharge = 50;
+        double balanceAfter1 = balances[4] + recharge;
+
+        if (balanceAfter1 >= 0) {
+
+            System.out.println(ownerNames[4] + "Total recharge = " + recharge + "  Refilled balance = " + balanceAfter1);
+        }
+    }
+}
