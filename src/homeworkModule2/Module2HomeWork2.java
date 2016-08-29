036a9bce970f1a24975f0c143bcac14944367d5b
DGreenleaf <serg.savitskiy@gmail.com>
2016-08-29 21:23:49 +0300

    changes to homework 2.2 - new changes

--- src/homeworkModule2/Module2HomeWork2.java
+++ src/homeworkModule2/Module2HomeWork2.java
@@ -2,48 +2,30 @@ package homeworkModule2;
 
 public class Module2HomeWork2 {
 
-    // всегда задаем корректные названия методов (withdraw) в
-    // соответствии с поставленной задачей
-    // так как можем снимать любую сумму, то тип данных int не подходит
+    public static void main (String[] args){
 
-    // double balance и withdrawal - переменные
+        double withdrawal = 50;
 
-    /*
-    static double withdraw(double balance, double withdrawal, double commissionFinal) {
-        // double commission = 5% = 0,05;
-        return balance - withdrawal - commissionFinal;
-    }
-    */
+        double commission = 0.05 * withdrawal;
 
-    public static void main(String[] args) {
+        double balanceAfter = withdrawBalance(500, withdrawal);
 
-        // balance - сумма денег на счету
-        double balance = 150;
-        // withdraw - то, что снимаем со счета
-        double withdrawal = 10.3;
-        // commission - комиссия при снятии
-        double commission = 0.05;
-        // можно сразу писать численные значения вместо методов (balance, withdraw, commission)
-        double commissionFinal = commission * withdrawal;
-        double outputMoney = 0;
-
-        System.out.println("OK " + commissionFinal + " " + output(balance, withdrawal, commissionFinal, outputMoney));
-    }
-
-    static double output(double balance, double commissionFinal, double withdrawal, double outputMoney) {
-
-        if (withdrawal + commissionFinal < balance) {
-            withdrawal = withdrawal + commissionFinal;
-            outputMoney = balance - withdrawal;
+        if (balanceAfter >= 0){
+            System.out.println("OK " + "Balance = " + balanceAfter + " Commission = " + commission);
         }
-        if (withdrawal + commissionFinal > balance ) {
-            System.out.print("NO");
+        if (balanceAfter <= 0) {
+            System.out.println("NO ");
         }
 
-        return outputMoney;
     }
 
+    static double withdrawBalance (double balance, double withdrawal) {
 
+        double commission = 0.05 * withdrawal;
+
+        return balance - withdrawal - commission;
+
+    }
 }
 
 
