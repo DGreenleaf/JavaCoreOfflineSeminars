83181903c4ee73a373c267e9fcc029e5f8aba800
DGreenleaf <serg.savitskiy@gmail.com>
2016-09-04 18:21:56 +0300

    homework 3_2  - changes

---
+++ src/homeworkModule3/HW2/Adder.java
@@ -0,0 +1,15 @@
+package homeworkModule3.HW2;
+
+
+public class Adder extends Arithmetic{
+
+    boolean check (Integer a, Integer b) {
+        if (a>=b) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
---
+++ src/homeworkModule3/HW2/Arithmetic.java
@@ -0,0 +1,14 @@
+package homeworkModule3.HW2;
+
+
+public class Arithmetic {
+
+    Integer a;
+    Integer b;
+
+    int add (Integer a, Integer b) {
+        return  a + b;
+    }
+
+
+}
