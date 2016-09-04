01d8421ee754c9acf05340cfe77fa15152599277
DGreenleaf <serg.savitskiy@gmail.com>
2016-09-04 18:18:31 +0300

    homework 3_2  - added

---
+++ src/homeworkModule3/HW2/Main.java
@@ -0,0 +1,28 @@
+package homeworkModule3.HW2;
+
+/*
+
+ Write class named Arithmetic with a method named add that takes
+ integers as parameters and returns an integer denoting their sum.
+
+ Write class named Adder that inherits from a superclass named Arithmetic.
+ Also class Adder must contain method check that compare two numbers and returns true if a>=b and false in other cases.
+
+ Methods signature:
+ int add(Integer a, Integer b)
+ boolean check(Integer a, Integer b)
+
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Adder result = new Adder();
+
+        System.out.println(result.check(120, 90));
+        System.out.println(result.add(120, 90));
+
+    }
+
+
+}
