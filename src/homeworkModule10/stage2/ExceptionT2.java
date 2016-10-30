a902bc61587aa3eb2bd68d6cedc4ccfb4a8656f9
DGreenleaf <serg.savitskiy@gmail.com>
2016-10-31 00:44:41 +0200

    HW M10 - 2nd

--- src/homeworkModule10/stage2/ExceptionT2.java
+++ src/homeworkModule10/stage2/ExceptionT2.java
@@ -3,18 +3,21 @@ package homeworkModule10.stage2;
 /**
  * Created by Sergiy on 10/31/16.
  */
-public class ExceptionT2 {
-
+public class ExceptionT2{
 
     public static void main(String[] args) throws Exception {
 
         try {
-            throw new MyException("This is my error");
+            throw new MyException("New error");
+
         } catch (MyException e) {
             System.err.println(e.getMessage());
+
         } finally {
             System.out.println("In finally block");
         }
+
+
     }
 }
 
--- src/homeworkModule10/stage2/MyException.java
+++ src/homeworkModule10/stage2/MyException.java
@@ -8,11 +8,32 @@ public class MyException extends Exception {
         this.message = message;
     }
 
+    public MyException(String message, String message1) {
+        super(message);
+        this.message = message1;
+    }
+
+    public MyException(String message, Throwable cause, String message1) {
+        super(message, cause);
+        this.message = message1;
+    }
+
+    public MyException(Throwable cause, String message) {
+        super(cause);
+        this.message = message;
+    }
+
+    public MyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, String message1) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.message = message1;
+    }
+
+    @Override
     public String getMessage() {
         return message;
     }
 
-    public void setMessage(String massage) {
+    public void setMessage(String message) {
         this.message = message;
     }
 }
