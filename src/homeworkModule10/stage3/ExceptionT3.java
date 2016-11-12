a0f6e229a81ab6d8732846d7ab96af6e13250b9d
DGreenleaf <serg.savitskiy@gmail.com>
2016-11-12 21:00:54 +0200

    HW M10 - stage3_2

--- src/homeworkModule10/stage3/ExceptionT3.java
+++ src/homeworkModule10/stage3/ExceptionT3.java
@@ -2,17 +2,24 @@ package homeworkModule10.stage3;
 
 import homeworkModule7.stage1.User;
 
-/**
- * Created by Sergiy on 10/31/16.
+
+
+/*
+ Define an object reference and initialize it to null.
+ Try to call a method through this reference.
+ Now wrap the code in a try-catch clause to catch the exception.
  */
+
 public class ExceptionT3 {
 
     public static void main(String[] args) {
         User user = null;
 
+//        user.getId();
+
         try {
             user.getId();
-        } catch (Throwable e) {
+        } catch (NullPointerException e) {
             System.err.println("Your user isn't found");
         } finally {
             user = new User(345, "Darren", "Greenleaf", "New York", 50000);
